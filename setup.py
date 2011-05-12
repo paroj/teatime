df059e8f3ef16d262133472629da92cb01bfd051
Pavel Rojtberg <pavel@rojtberg.net>
2011-05-12 15:42:43 +0200

    * add setup.py der bzr

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from DistUtilsExtra.command import *
+
+setup(
+      cmdclass = {"build": build_extra.build_extra,
+                  "build_i18n":  build_i18n.build_i18n},
+      name = "teatime",
+      version = "11.04",
+      description = "a tea timer",
+      author = "Pavel Rojtberg",
+      author_email = "pavel@rojtberg.net",
+      url = "http://www.rojtberg.net/",
+      license = "GNU GPL v3",
+      scripts = ["teatime.py"],
+      data_files = [("share/applications/", ["teatime.desktop"]),
+                    ("share/teatime/", ["window.ui"])])
