0c2e2e6739a0eecfff61fc278dc0686d5fa2924b
Pavel Rojtberg <pavel@rojtberg.net>
2015-01-03 15:54:54 +0100

    setup.py: fix DEST location for locale installation

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from DistUtilsExtra.command import *
 
 #DEST="/opt/extras.ubuntu.com/teatime/"
-DEST="share/teatime"
+DEST="share/teatime/"
 
 class my_build_i18n(build_i18n.build_i18n):
     def run(self):
